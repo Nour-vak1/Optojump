4d5ab58b10c9e59968f3bb6dc39d91f27296de88
Pulkio <guillaume.cobat@gmx.fr>
2024-03-07 20:31:22 +0100

    modif ui

--- modules/CMJ.R
+++ modules/CMJ.R
@@ -49,7 +49,6 @@ CMJ_Server <- function(input, output, session) {
   observeEvent(input$submit_button, {
     req(input$xml_file, input$athlete_name, input$gender_select)
     
-    
     # Exemple d'utilisation
     xml_file <- input$xml_file$datapath
     columns_to_extract <- c("Elevation")
@@ -58,18 +57,15 @@ CMJ_Server <- function(input, output, session) {
     result <- parse_xml_file(xml_file, columns_to_extract)
     
     # Si préférence résultats en ligne
-    
     ###############################################
     
     if (!is.null(result) && length(result) > 0) {
-      
       result <- as.data.frame(t(result))
       # Définir les noms de colonnes avec la première ligne
       names(result) <- unlist(result[1, ])
       # Supprimer la première ligne car elle est maintenant utilisée comme noms de colonnes
       result <- result[-1, ]
       result <- as.numeric(result)
-      
       
       # Imprimer la plus grande valeur
       max_value <- max(result)
@@ -97,10 +93,7 @@ CMJ_Server <- function(input, output, session) {
     }
     
     ##############################################
-    
   })
-
-
   
   output$bar_chart <- renderPlotly({
     req(input$xml_file, input$athlete_name, input$gender_select, isDataProcessed())
@@ -108,62 +101,28 @@ CMJ_Server <- function(input, output, session) {
       donnees <- read.csv("results_globaux_CMJ.csv")
       
       if (!is.null(donnees) && nrow(donnees) > 0) {
-        
         hauteur_record <- max(donnees$Hauteur)
         data_standeur <- donnees[nrow(donnees), ]
         record_nour <- 59 # Valeur temporaire
         
-        largeur <- c(0.4, 0.4) # Augmentez la largeur des barres
+        fig <- plot_ly()
         
-        fig <- plot_ly(donnees, x = ~Test, type = 'bar', width = largeur)
+        fig <- fig %>% add_trace(x = c("Record de Nour"), y = c(record_nour), name = 'Record de Nour', type = 'bar', marker = list(color = '#DCC283'))
+        fig <- fig %>% add_trace(x = c("Record du salon"), y = c(hauteur_record), name = 'Record du salon', type = 'bar', marker = list(color = '#C5243D'))
+        fig <- fig %>% add_trace(x = c("Votre performance"), y = c(data_standeur$Hauteur), name = 'Votre performance', type = 'bar', marker = list(color = '#2C2F65'))
         
-        fig <- fig %>% add_trace(y = ~record_nour, name = 'Record de Nour', marker = list(color = '#DCC283'), text = paste(round(record_nour, 2), "cm"), showlegend = FALSE)
+        # Ajouter le texte au milieu de chaque barre
+        fig <- fig %>% add_text(x = c("Record de Nour", "Record du salon", "Votre performance"),
+                                y = c(record_nour/2, hauteur_record/2, data_standeur$Hauteur/2), # Diviser par 2 pour centrer verticalement
+                                text = paste0(c(record_nour, hauteur_record, data_standeur$Hauteur), " cm"),
+                                textposition = "auto", # Laisser plotly décider de l'alignement horizontal
+                                showlegend = FALSE,
+                                textfont = list(color = "white", size = 14))
         
-        fig <- fig %>% add_trace(y = ~hauteur_record, name = 'Record du salon', marker = list(color = '#C5243D'), text = paste(round(hauteur_record, 2), "cm"), showlegend = FALSE)
-        
-        fig <- fig %>% add_trace(y = ~data_standeur$Hauteur, name = 'Votre performance', marker = list(color = '#2C2F65'), text = paste(round(data_standeur$Hauteur, 2), "cm"), showlegend = FALSE)
-        
-        moyenne <- mean(donnees$Hauteur)
-        
-        fig <- fig %>% layout(yaxis = list(title = 'Hauteur'), barmode = 'overlay',
-                              margin = list(t = 50, b = 50))
-        
-        hline <- function(y = moyenne, color = "#C5243D") {
-          list(
-            type = "line",
-            x0 = 0,
-            x1 = 1,
-            xref = "paper",
-            y0 = y,
-            y1 = y,
-            yref = "y",
-            line = list(color = color, dash = "dot", width = 2)
-          )
-        }
-        
-        fig <- fig %>% layout(
-          shapes = list(
-            hline(record_nour, "#F9E9CA"),
-            hline(hauteur_record, "#C5243D"),
-            hline(data_standeur$Hauteur, "#2C2F65")
-          ),
-          yaxis = list(title = 'Hauteur'),
-          barmode = 'overlay',
-          margin = list(t = 50, b = 50),
-          showlegend = FALSE # Supprime la légende
-        )
-        
-        # Ajouter des légendes personnalisées au-dessus de chaque ligne
-        fig <- fig %>% add_annotations(
-          x = c(1, 1, 1),
-          y = c(record_nour + 1, hauteur_record + 1, data_standeur$Hauteur + 1), # Ajuste la position verticale des annotations
-          text = c("Record de Nour", "Performance Record", "Votre performance"),
-          showarrow = FALSE,
-          xanchor = "right",
-          yanchor = "bottom", # Ancre les annotations en bas
-          xshift = -20,
-          font = list(color = c("#F9E9CA", "#C5243D", "#2C2F65"))
-        )
+        fig <- fig %>% layout(yaxis = list(title = 'Hauteur'),
+                              barmode = 'group', # Pour afficher les barres côte à côte
+                              margin = list(t = 50, b = 50),
+                              showlegend = TRUE) # Afficher la légende
         
         return(fig)
       } else {
@@ -176,10 +135,7 @@ CMJ_Server <- function(input, output, session) {
   
   
   
-  
-  
-  
-  output$results_text <- renderUI({  # Modification ici
+  output$results_text <- renderUI({
     req(input$xml_file, input$athlete_name, input$gender_select, isDataProcessed())
     if (file.exists("results_globaux_CMJ.csv")) {
       donnees <- read.csv("results_globaux_CMJ.csv")
@@ -195,13 +151,10 @@ CMJ_Server <- function(input, output, session) {
           tags$strong(style="font-size:2.2em;","Résultats"), tags$br(), tags$br(),
           tags$span(style="font-size:2.2em;", "Hauteur record Nour : "), tags$strong(style="font-size:2.2em;", round(hauteur_record, 2)), tags$strong(style="font-size:2.2em;"," cm"), tags$br(), tags$br(),
           tags$span(style="font-size:2.2em;", "Hauteur visiteur : "), tags$strong(style="font-size:2.2em;", round(data_standeur$Hauteur, 2)), tags$strong(style="font-size:2.2em;"," cm"), tags$br(), tags$br(),
-          tags$span(style="font-size:2.2em;", "Moyenne : "), tags$strong(style="font-size:2.2em;", round(moyenne, 2)), tags$strong(style="font-size:2.2em;"," cm")
+          tags$span(style="font-size:2.2em;", "Moyenne du salon : "), tags$strong(style="font-size:2.2em;", round(moyenne, 2)), tags$strong(style="font-size:2.2em;"," cm")
         )
         
-        
-        # Retourner le texte des résultats
-        return(result_text)  # Modification ici
-        
+        return(result_text)
       } else {
         return("Le fichier CSV est vide.")
       }
@@ -209,8 +162,4 @@ CMJ_Server <- function(input, output, session) {
       return("Le fichier CSV n'existe pas.")
     }
   })
-  
-  
-  
-
 }
